8a4a54799a78a5e3be8950d189bad35d66f76828
TheEdward162 <theedward162@gmail.com>
2019-03-24 20:14:03 +0100

    status serialization

--- src/main/kotlin/cz/applifting/edward/MonitoringService/resource/Status.kt
+++ src/main/kotlin/cz/applifting/edward/MonitoringService/resource/Status.kt
@@ -1,5 +1,10 @@
 package cz.applifting.edward.MonitoringService
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 enum class Status(val code: Int, val message: String) {
 	OK(200, "Ok"),
 
@@ -7,5 +12,20 @@ enum class Status(val code: Int, val message: String) {
 	Unauthorized(401, "Unauthorized"),
 	NotFound(404, "Not Found"),
 
-	ServerError(500, "Server Error")
+	ServerError(500, "Server Error");
+
+	companion object {
+        @JvmStatic @JsonCreator
+        fun fromCode(@JsonProperty("code") code: Int): Status {
+			when (code) {
+				200 -> return Status.OK
+
+				400 -> return Status.BadRequest
+				401 -> return Status.Unauthorized
+				404 -> return Status.NotFound
+
+				else -> return Status.ServerError
+			}
+        }
+    }
 }
